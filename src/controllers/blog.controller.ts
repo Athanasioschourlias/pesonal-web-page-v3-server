b6fa6d0634ac31f9a7e62a5fa6935a42f2feeed7
Athanasioschourlias <thanos.chourlias@gmail.com>
2022-09-01 14:26:36 +0300

    set-up/PWPS-24
      * Fixes with the docker file
      * Added the db
      * Created the db's blog table model/type
      * Added migration manager
      * Created the first migration for the blog table(creating the table)
      * FIxed docker isssues in order for the server and db to run.

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -2,21 +2,21 @@ import {Request, Response} from "express"
 
 export async function getAllArticles(_req: Request, res: Response): Promise<void> {
 
-	res.statusCode = 404
+	res.statusCode = 200
 	res.send("Oupsss We dont have that functionality yet try again an other time")
 
 }
 
 export async function deleteArticles(_req: Request, res: Response): Promise<void> {
 
-	res.statusCode = 404
+	res.statusCode = 200
 	res.send("Oupsss We dont have that functionality yet try again an other time")
 
 }
 
 export async function editArticles(_req: Request, res: Response): Promise<void> {
 
-	res.statusCode = 404
+	res.statusCode = 200
 	res.send("Oupsss We dont have that functionality yet try again an other time")
 
 }
--- src/types/serviceGenericTypes.ts
+++ src/types/serviceGenericTypes.ts
@@ -1,12 +1,18 @@
 
 //Article types
-
 type  article = {
 	title: string,
 	article_description: string,
+	//The images attributes will be just the name of the image that is already
+	// uploaded or will be sanctimoniously uploaded with a standar prefix that
+	// points to the clients articles folder with all the photos.
 	first_img: string,
 	second_img: string,
-	tech_specification_klist: string[]
+	in_service: string,
+	mcu: string,
+	relative_hardware: string,
+	software: string,
+	libraries_used: string
 
 }
 
