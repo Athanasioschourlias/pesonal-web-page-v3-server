03d4342fcecff4758c751ccdbeabe91cdc0b5a3b
Chris Maikos <chmaikos@gmail.com>
2023-09-10 19:56:17 +0300

    devops * make linter fix what is fixable

--- src/service/authentication.service.ts
+++ src/service/authentication.service.ts
@@ -6,70 +6,57 @@ import process from "process"
 import jwt from "jsonwebtoken"
 import {login_creds, verifiedUser} from "../types/authentication types"
 
-export async function register(user: User): Promise<string | null> {
+export async function register(user: User): Promise<string> {
 	try {
 		const res = await collections.users?.findOne({ name: user.username })
-		if(!res) {
-			return "There is another user with the same credentials"
+		if(res) {
+			throw new Error("There is another user with the same credentials")
 		}
-
-		try {
-			const password = await bcrypt.hash(user.password, 8)
-
-			if(!collections.users) {
-				throw new Error("The collection is missing from the database")
-			}
-
-			const userRes = await collections.users.insertOne({
-				name: user.username,
-				role: user.role,
-				password: password,
-			})
-
-			logger.info(userRes)
-			return "The user was created successfully"
-		} catch (error) {
-			logger.error(`Operation failed -> ${error}`)
-			return Promise.reject(`Operation failed -> ${error}`)
-		}
-	} catch (err) {
-		logger.error(`There was a problem creating the user ${err}`)
-		return Promise.reject(`There was a problem creating the user ${err}`)
-	}
-}
-
-export async function login(user: login_creds): Promise<string | null | verifiedUser> {
-	try {
-		if(!user) {
-			throw new Error("No user details provided")
-		}
-
+		
+		const password = await bcrypt.hash(user.password, 8)
+		
 		if(!collections.users) {
 			throw new Error("The collection is missing from the database")
 		}
 
-		const foundUser = await collections.users.findOne({ name: user.username })
+		const userRes = await collections.users.insertOne({
+			name: user.username,
+			role: user.role,
+			password: password,
+		})
 
-		if(!foundUser) {
-			throw new Error("There is no such user with that username")
-		}
-
-		const isMatch = bcrypt.compareSync(user.password, foundUser.password)
-
-		if(isMatch) {
-			const token = jwt.sign({ _id: foundUser._id?.toString(), name: foundUser.username },
-				process.env.TOKEN_SECRET as string, {
-					expiresIn: "2 days",
-				})
-
-			return { user: { username: foundUser.username, role: foundUser.role }, token: token }
-		} else {
-			throw new Error("Password is not correct")
-		}
-	} catch (error: unknown) {
-		logger.error(`Login failed -> ${(error as Error).message}`)
-		throw error
+		logger.info(userRes)
+		return "The user was created successfully"
+	} catch (error: any) {
+		throw new Error(`Operation failed -> ${error}`)
 	}
+}
+
+async function createJWT(id: string, username: string): Promise<string> {
+	return jwt.sign({ _id: id, name: username }, process.env.TOKEN_SECRET as string, {
+		expiresIn: "2 days",
+	})
+}
+
+async function comparePassword(providedPassword: string, storedPassword: string): Promise<boolean> {
+	return bcrypt.compareSync(providedPassword, storedPassword)
+}
+
+export async function login(user: login_creds): Promise<verifiedUser> {
+	if(!user) throw new Error("No user details provided")
+
+	if(!collections.users) throw new Error("The collection is missing from the database")
+
+	const foundUser = await collections.users.findOne({ name: user.username })
+
+	if(!foundUser) throw new Error("There is no such user with that username")
+
+	const isMatch = await comparePassword(user.password, foundUser.password)
+	if(!isMatch) throw new Error("Password is not correct")
+
+	const token = await createJWT(foundUser._id?.toString() ?? "", foundUser.username)
+
+	return { user: { username: foundUser.username, role: foundUser.role }, token }
 }
 
 export async function __createAdmin(user: User): Promise<string> {
@@ -91,9 +78,8 @@ export async function __createAdmin(user: User): Promise<string> {
 		})
 
 		logger.info(userRes)
-		return "The user was created successfully"
+		return "The admin was created successfully"
 	} catch (error: unknown) {
-		logger.error(`Operation failed -> ${String(error)}`)
 		throw new Error(`Operation failed -> ${String(error)}`)
 	}
 }
