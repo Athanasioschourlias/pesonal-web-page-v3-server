97dedc2799bbda6eba28316758648aa7760f6dc1
Athanasioschourlias <thanos.chourlias@gmail.com>
2023-09-10 19:00:17 +0300

    bug-fix:
    * fixed minor typing and flow issues in the authentication service file

--- src/service/authentication.service.ts
+++ src/service/authentication.service.ts
@@ -7,134 +7,98 @@ import jwt from "jsonwebtoken"
 import {login_creds, verifiedUser} from "../types/authentication types"
 
 export async function register(user: User): Promise<string | null> {
+	try {
+		const res = await collections.users?.findOne({ name: user.username })
+		if(!res) {
+			return "There is another user with the same credentials"
+		}
 
-	collections.users?.findOne({
-		name: user.username
-	}).then((res) => {
-		if(res)
-			return new Promise<string | null>(resolve => resolve("There is an other user with the same credentials"))
-		else
-			return new Promise<string | null>((resolve, reject) => {
+		try {
+			const password = await bcrypt.hash(user.password, 8)
 
-				//hashing the password and storing it to the users object
-				bcrypt.hash(user.password, 8).then((password) => {
-
-					if(!collections.users)
-						return reject("The collection is missing from the database")
-
-					collections.users.insertOne({
-						name: user.username,
-						role: user.role,
-						password: password
-					}).then((user) => {
-						logger.info(user)
-						if(user)
-							return resolve("The user was created successfully")
-						else
-							return reject("The user could not be created")
-					}).catch((error: Error) => {
-						logger.error(`Adding user to database failed -> ${error}`)
-						return reject(`Adding user to database failed -> ${error}`)
-					})
-				}).catch((error: Error) => {
-					logger.error(`Password hashing failed -> ${error}`)
-					return reject(`Password hashing failed -> ${error}`)
-				})
-
-			})
-	}).catch((err) => {
-		return new Promise<string | null>(reject => reject(`There was a problem creating the user ${err}`))
-	})
-
-	logger.error("This is a promise that should never be returned fro registering the admin")
-	return new Promise<string | null>(
-		reject => reject("This is a promise that should never be returned fro registering the admin")
-	)
-}
-
-
-export async function login(user: login_creds): Promise<string | null | verifiedUser> {
-
-	//Checking is the user provides a user or not
-	if(!user)
-		return new Promise(reject => reject("No user details provided"))
-
-	return new Promise((_resolve, reject) => {
-
-		if(!collections.users)
-			return reject("The collection is missing from the database")
-
-		//searching for the user
-		collections.users.findOne({
-			name: user.username
-		}).then((foundUser) => {
-
-			if(!foundUser) {
-				return reject("There is no such user with username")
+			if(!collections.users) {
+				throw new Error("The collection is missing from the database")
 			}
 
+			const userRes = await collections.users.insertOne({
+				name: user.username,
+				role: user.role,
+				password: password,
+			})
 
-			const isMatch = bcrypt.compareSync(user.password, foundUser.password)
+			logger.info(userRes)
+			return "The user was created successfully"
+		} catch (error) {
+			logger.error(`Operation failed -> ${error}`)
+			return Promise.reject(`Operation failed -> ${error}`)
+		}
+	} catch (err) {
+		logger.error(`There was a problem creating the user ${err}`)
+		return Promise.reject(`There was a problem creating the user ${err}`)
+	}
+}
 
+export async function login(user: login_creds): Promise<string | null | verifiedUser> {
+	try {
+		if(!user) {
+			throw new Error("No user details provided")
+		}
 
-			if(isMatch) {
-				// eslint-disable-next-line max-len
-				const token = jwt.sign({ _id: foundUser._id?.toString(), name: foundUser.username }, process.env.TOKEN_SECRET, {
+		if(!collections.users) {
+			throw new Error("The collection is missing from the database")
+		}
+
+		const foundUser = await collections.users.findOne({ name: user.username })
+
+		if(!foundUser) {
+			throw new Error("There is no such user with that username")
+		}
+
+		const isMatch = bcrypt.compareSync(user.password, foundUser.password)
+
+		if(isMatch) {
+			const token = jwt.sign({ _id: foundUser._id?.toString(), name: foundUser.username },
+				process.env.TOKEN_SECRET as string, {
 					expiresIn: "2 days",
 				})
 
-				return _resolve(
-					{ user: { username: foundUser.username, role: foundUser.role}, token: token }
-				)
-			} else {
-				reject("Password is not correct")
-			}
-
-		}).catch((error: Error) => {
-			logger.error(`There was an error while hashing -> ${error}`)
-		})
-
-	})
+			return { user: { username: foundUser.username, role: foundUser.role }, token: token }
+		} else {
+			throw new Error("Password is not correct")
+		}
+	} catch (error: unknown) {
+		logger.error(`Login failed -> ${(error as Error).message}`)
+		throw error
+	}
 }
 
-export async function __createAdmin(user: User): Promise<void> {
+export async function __createAdmin(user: User): Promise<string | null> {
+	try {
+		const res = await collections.users?.findOne({ name: user.username })
+		if(res) {
+			return "There is another user with the same credentials"
+		} else {
+			try {
+				const password = await bcrypt.hash(user.password, 8)
+				if(!collections.users) {
+					throw new Error("The collection is missing from the database")
+				}
 
-	collections.users?.findOne({
-		name: user.username
-	}).then((res) => {
-		if(res)
-			return new Promise<string | null>(resolve => resolve("There is an other user with the same credentials"))
-		else
-			return new Promise<string | null>((resolve, reject) => {
-
-				//hashing the password and storing it to the users object
-				bcrypt.hash(user.password, 8).then((password) => {
-
-					if(!collections.users)
-						return reject("The collection is missing from the database")
-
-					collections.users.insertOne({
-						name: user.username,
-						role: user.role,
-						password: password
-					}).then((user) => {
-						logger.info(user)
-						if(user)
-							return resolve("The user was created successfully")
-						else
-							return reject("The user could not be created")
-					}).catch((error: Error) => {
-						logger.error(`Adding user to database failed -> ${error}`)
-						return reject(`Adding user to database failed -> ${error}`)
-					})
-				}).catch((error: Error) => {
-					logger.error(`Password hashing failed -> ${error}`)
-					return reject(`Password hashing failed -> ${error}`)
+				const userRes = await collections.users.insertOne({
+					name: user.username,
+					role: user.role,
+					password: password,
 				})
 
-			})
-	}).catch((err) => {
-		return new Promise<string | null>(reject => reject(`There was a problem creating the user ${err}`))
-	})
-
+				logger.info(userRes)
+				return "The user was created successfully"
+			} catch (error: unknown) {
+				logger.error(`Operation failed -> ${String(error)}`)
+				return Promise.reject(`Operation failed -> ${String(error)}`)
+			}
+		}
+	} catch (err: unknown) {
+		logger.error(`There was a problem creating the user ${String(err)}`)
+		return Promise.reject(`There was a problem creating the user ${String(err)}`)
+	}
 }
