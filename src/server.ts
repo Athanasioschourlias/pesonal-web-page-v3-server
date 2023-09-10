656bfffb0b1ae3bb1667854a4382c580b0adfd72
Athanasioschourlias <thanos.chourlias@gmail.com>
2023-09-10 18:55:42 +0300

    code-cleanup:
    * register bug fix

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,6 @@ import routes from "./routes"
 import logger from "./common/logger"
 import packageJson from "../package.json"
 import config from "./config/config"
-import {existsSync} from "fs"
-import path from "path"
 import {connectToDatabase} from "./service/database.service"
 import cors from "cors"
 import {__createAdmin} from "./service/authentication.service"
@@ -49,29 +47,6 @@ connectToDatabase()
 		})
 	})
 	.catch((error: Error) => {
-		logger.error(`Databae connection failed -> ${error}`)
+		logger.error(`Database connection failed -> ${error}`)
 	})
-
-//Error Handler
-//TODO-implement an error handler
-
-/**
- * Here multiple things are happening:
- * Firstly, we are checking(synchronously) if a file exists in the given path
- * Then, we use the build in middleware of js to let express know there is a dist folder and assets of the vue build
-   static files from a directory located at the root of our project and its named dist.
- * Then for the default URI(/) we are loading the index.html file.
- **/
-if(existsSync(path.join(__dirname, "../public"))) {
-
-	app.use(express.static(path.join(__dirname, "../public")))
-	logger.info("Serving Static Files")
-
-	app.get("/", (_req, res) => {
-		res.sendFile(path.join(__dirname, "../public/index.html"))
-		logger.info("Serving Static Files")
-
-	})
-}
-
 
